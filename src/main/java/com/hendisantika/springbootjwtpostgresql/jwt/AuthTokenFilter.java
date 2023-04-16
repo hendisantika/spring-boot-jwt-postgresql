1c3eda59d5d0f7dfc067d39058601c1b5f6d5587
hendisantika <hendisantika@yahoo.co.id>
2023-04-16 09:56:38 +0700

    #29 - Bug Fix code & lib ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootjwtpostgresql/jwt/AuthTokenFilter.java
+++ src/main/java/com/hendisantika/springbootjwtpostgresql/jwt/AuthTokenFilter.java
@@ -5,8 +5,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -26,12 +26,13 @@ import java.io.IOException;
  * Time: 09:47
  * To change this template use File | Settings | File Templates.
  */
-@RequiredArgsConstructor
 @Slf4j
 public class AuthTokenFilter extends OncePerRequestFilter {
-    private final JwtUtils jwtUtils;
+    @Autowired
+    private JwtUtils jwtUtils;
 
-    private final UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
