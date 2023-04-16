4bf01081a75f29aa503cc05c736b47708af1a10e
hendisantika <hendisantika@yahoo.co.id>
2023-04-16 09:51:55 +0700

    #28 - Add TestController ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootjwtpostgresql/config/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/springbootjwtpostgresql/config/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.hendisantika.springbootjwtpostgresql.config;
 
+import com.hendisantika.springbootjwtpostgresql.jwt.AuthEntryPointJwt;
+import com.hendisantika.springbootjwtpostgresql.jwt.AuthTokenFilter;
+import com.hendisantika.springbootjwtpostgresql.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/com/hendisantika/springbootjwtpostgresql/controller/TestController.java
@@ -0,0 +1,45 @@
+package com.hendisantika.springbootjwtpostgresql.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/16/23
+ * Time: 09:51
+ * To change this template use File | Settings | File Templates.
+ */
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+    @GetMapping("/all")
+    public String allAccess() {
+        return "Public Content.";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String userAccess() {
+        return "User Content.";
+    }
+
+    @GetMapping("/mod")
+    @PreAuthorize("hasRole('MODERATOR')")
+    public String moderatorAccess() {
+        return "Moderator Board.";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        return "Admin Board.";
+    }
+}
