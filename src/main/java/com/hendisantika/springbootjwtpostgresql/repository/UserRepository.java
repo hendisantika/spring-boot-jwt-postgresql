4d009eed401672a0b0f29ed2b34a446342e3faae
hendisantika <hendisantika@yahoo.co.id>
2023-04-16 09:41:20 +0700

    #12 - Add UserRepository ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootjwtpostgresql/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package com.hendisantika.springbootjwtpostgresql.repository;
+
+import com.hendisantika.springbootjwtpostgresql.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/16/23
+ * Time: 09:41
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
