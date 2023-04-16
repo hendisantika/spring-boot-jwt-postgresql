9e0269993fe1c4f5d712387443d70c1afaa0e096
hendisantika <hendisantika@yahoo.co.id>
2023-04-16 09:46:10 +0700

    #20 - Add UserDetailsServiceImpl Service ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootjwtpostgresql/service/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.hendisantika.springbootjwtpostgresql.service;
+
+import com.hendisantika.springbootjwtpostgresql.model.User;
+import com.hendisantika.springbootjwtpostgresql.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/16/23
+ * Time: 09:45
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
