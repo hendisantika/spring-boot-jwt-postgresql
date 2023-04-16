ef606c879a120097b8725cb0f932ff677f1dff7f
hendisantika <hendisantika@yahoo.co.id>
2023-04-16 09:30:56 +0700

    #3 - Add ERole Enum Model ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootjwtpostgresql/model/ERole.java
@@ -0,0 +1,17 @@
+package com.hendisantika.springbootjwtpostgresql.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/16/23
+ * Time: 09:30
+ * To change this template use File | Settings | File Templates.
+ */
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
