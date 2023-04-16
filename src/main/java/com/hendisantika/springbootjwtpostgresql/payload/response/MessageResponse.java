b2cc346b26c4aa6e1f9549e5627209bebc5de478
hendisantika <hendisantika@yahoo.co.id>
2023-04-16 09:39:53 +0700

    #10 - Add MessageResponse payload ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootjwtpostgresql/payload/response/MessageResponse.java
@@ -0,0 +1,20 @@
+package com.hendisantika.springbootjwtpostgresql.payload.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/16/23
+ * Time: 09:39
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class MessageResponse {
+    private String message;
+}
