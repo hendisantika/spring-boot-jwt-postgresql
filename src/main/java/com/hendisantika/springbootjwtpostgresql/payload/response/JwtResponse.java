0e16fa653e837a3c66df9af046409640a51ee4e2
hendisantika <hendisantika@yahoo.co.id>
2023-04-16 09:39:17 +0700

    #9 - Add JwtResponse payload ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootjwtpostgresql/payload/response/JwtResponse.java
@@ -0,0 +1,35 @@
+package com.hendisantika.springbootjwtpostgresql.payload.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/16/23
+ * Time: 09:37
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+    private List<String> roles;
+
+    public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
+        this.token = accessToken;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+}
