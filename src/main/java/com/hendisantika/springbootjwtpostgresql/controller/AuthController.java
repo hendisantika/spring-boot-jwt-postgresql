b3d4b18915a63162fc5decef66db0f8d579c1c32
hendisantika <hendisantika@yahoo.co.id>
2023-04-16 23:12:37 +0700

    #31 - Add authenticateUser in AuthController ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootjwtpostgresql/controller/AuthController.java
+++ src/main/java/com/hendisantika/springbootjwtpostgresql/controller/AuthController.java
@@ -1,14 +1,23 @@
 package com.hendisantika.springbootjwtpostgresql.controller;
 
 import com.hendisantika.springbootjwtpostgresql.jwt.JwtUtils;
+import com.hendisantika.springbootjwtpostgresql.payload.request.LoginRequest;
+import com.hendisantika.springbootjwtpostgresql.payload.response.JwtResponse;
 import com.hendisantika.springbootjwtpostgresql.repository.RoleRepository;
 import com.hendisantika.springbootjwtpostgresql.repository.UserRepository;
+import com.hendisantika.springbootjwtpostgresql.service.UserDetailsImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -34,4 +43,25 @@ public class AuthController {
     private final PasswordEncoder encoder;
 
     private final JwtUtils jwtUtils;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
+    }
 }
