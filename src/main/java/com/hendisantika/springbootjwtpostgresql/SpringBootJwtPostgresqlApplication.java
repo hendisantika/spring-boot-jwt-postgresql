6cf15f3f91cc16620d6b4be83bd72e6841a17c55
hendisantika <hendisantika@yahoo.co.id>
2023-04-16 08:43:14 +0700

    #1 - Initial commit ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootjwtpostgresql/SpringBootJwtPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootjwtpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtPostgresqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtPostgresqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootjwtpostgresql/SpringBootJwtPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootjwtpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtPostgresqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
