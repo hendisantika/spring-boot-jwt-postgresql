74dcb8697ccb104aeab7f8781fe033b7be0a372c
hendisantika <hendisantika@yahoo.co.id>
2023-04-16 09:49:16 +0700

    #24 - Add JwtUtils ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootjwtpostgresql/jwt/JwtUtils.java
@@ -0,0 +1,26 @@
+package com.hendisantika.springbootjwtpostgresql.jwt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/16/23
+ * Time: 09:48
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@Slf4j
+public class JwtUtils {
+
+    @Value("${hendi.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${hendi.app.jwtExpirationMs}")
+    private int jwtExpirationMs;
+}
