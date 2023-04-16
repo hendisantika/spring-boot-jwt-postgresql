71f9f4bbb3199aef3d38a836b6b834bcc33490e3
hendisantika <hendisantika@yahoo.co.id>
2023-04-16 09:43:36 +0700

    #16 - Add authenticationManager in WebSecurityConfig ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootjwtpostgresql/config/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/springbootjwtpostgresql/config/WebSecurityConfig.java
@@ -3,7 +3,9 @@ package com.hendisantika.springbootjwtpostgresql.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 /**
@@ -37,5 +39,10 @@ public class WebSecurityConfig {
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 }
