d7653a22b0677150ab64fabf66f96c63832ac873
hendisantika <hendisantika@yahoo.co.id>
2023-04-17 13:23:59 +0700

    #35 - Add getName API ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootjwtpostgresql/controller/AuthController.java
+++ src/main/java/com/hendisantika/springbootjwtpostgresql/controller/AuthController.java
@@ -1,18 +1,17 @@
 package com.hendisantika.springbootjwtpostgresql.controller;
 
 import com.hendisantika.springbootjwtpostgresql.jwt.JwtUtils;
-import com.hendisantika.springbootjwtpostgresql.model.ERole;
-import com.hendisantika.springbootjwtpostgresql.model.Role;
 import com.hendisantika.springbootjwtpostgresql.model.User;
+import com.hendisantika.springbootjwtpostgresql.payload.AuthToken;
 import com.hendisantika.springbootjwtpostgresql.payload.request.LoginRequest;
 import com.hendisantika.springbootjwtpostgresql.payload.request.SignupRequest;
 import com.hendisantika.springbootjwtpostgresql.payload.response.JwtResponse;
 import com.hendisantika.springbootjwtpostgresql.payload.response.MessageResponse;
-import com.hendisantika.springbootjwtpostgresql.repository.RoleRepository;
 import com.hendisantika.springbootjwtpostgresql.repository.UserRepository;
 import com.hendisantika.springbootjwtpostgresql.service.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -20,11 +19,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -45,85 +39,54 @@ public class AuthController {
 
     private final UserRepository userRepository;
 
-    private final RoleRepository roleRepository;
-
     private final PasswordEncoder encoder;
 
     private final JwtUtils jwtUtils;
 
     @PostMapping("/signin")
-    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) throws Exception {
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+                new UsernamePasswordAuthenticationToken(loginRequest.getPhoneNumber(), loginRequest.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = jwtUtils.generateJwtToken(authentication);
+        String jwt = jwtUtils.generateJwt(authentication);
 
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(item -> item.getAuthority())
-                .collect(Collectors.toList());
-
         return ResponseEntity.ok(new JwtResponse(jwt,
                 userDetails.getId(),
-                userDetails.getUsername(),
-                userDetails.getEmail(),
-                roles));
+                userDetails.getPhoneNumber()
+        ));
     }
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+        if (userRepository.existsByPhoneNumber(signUpRequest.getPhoneNumber())) {
             return ResponseEntity
                     .badRequest()
-                    .body(new MessageResponse("Error: Username is already taken!"));
-        }
-
-        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-            return ResponseEntity
-                    .badRequest()
-                    .body(new MessageResponse("Error: Email is already in use!"));
+                    .body(new MessageResponse("Error: Phone Number is already in use!"));
         }
 
         // Create new user's account
-        User user = new User(signUpRequest.getUsername(),
-                signUpRequest.getEmail(),
+        User user = new User(signUpRequest.getPhoneNumber(), signUpRequest.getName(),
                 encoder.encode(signUpRequest.getPassword()));
 
-        Set<String> strRoles = signUpRequest.getRole();
-        Set<Role> roles = new HashSet<>();
-
-        if (strRoles == null) {
-            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-            roles.add(userRole);
-        } else {
-            strRoles.forEach(role -> {
-                switch (role) {
-                    case "admin":
-                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                        roles.add(adminRole);
-
-                        break;
-                    case "mod":
-                        Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
-                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                        roles.add(modRole);
-
-                        break;
-                    default:
-                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                        roles.add(userRole);
-                }
-            });
-        }
-
-        user.setRoles(roles);
         userRepository.save(user);
-
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
     }
+
+    @GetMapping("/name")
+    public ResponseEntity<?> getName(@RequestHeader(name = "authorization") String authorization) throws JSONException {
+        AuthToken token = new AuthToken(authorization);
+
+        boolean isJWTExpired = jwtUtils.isJWTExpired(token.getDecodedJWT());
+        if (isJWTExpired) {
+            return ResponseEntity.ok(new MessageResponse("Token has been expired!"));
+        }
+
+        String phoneNumber = token.getPhoneNumber();
+        String name = token.getName();
+        return ResponseEntity.ok(new MessageResponse("Name: " + name));
+    }
+
+
 }
