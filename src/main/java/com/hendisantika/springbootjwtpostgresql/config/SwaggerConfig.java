4f6caff9f000c10226843e35ab8e1c8fbef1ac33
hendisantika <hendisantika@yahoo.co.id>
2023-04-18 09:31:26 +0700

    #43 - Add Security Scheme in SpringDoc ✏️📗🐳:octocat:

--- src/main/java/com/hendisantika/springbootjwtpostgresql/config/SwaggerConfig.java
+++ src/main/java/com/hendisantika/springbootjwtpostgresql/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.hendisantika.springbootjwtpostgresql.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
@@ -21,6 +23,12 @@ import org.springframework.context.annotation.Configuration;
  * To change this template use File | Settings | File Templates.
  */
 @Configuration
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class SwaggerConfig {
     @Bean
     public GroupedOpenApi publicApi() {
